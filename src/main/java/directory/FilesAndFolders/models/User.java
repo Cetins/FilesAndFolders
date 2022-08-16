34b22dff4a043a388fd13a3003c9a3a6719654ab
Cetins <69481095+Cetins@users.noreply.github.com>
2022-08-16 20:11:00 +0100

    project downloaded from spring, models added

---
+++ src/main/java/directory/FilesAndFolders/models/User.java
@@ -0,0 +1,68 @@
+package directory.FilesAndFolders.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany
+    @JsonIgnoreProperties({"users"})
+    @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
+    @JoinTable(
+            name = "user_folders",
+            joinColumns = { @JoinColumn(
+                    name = "user_id",
+                    nullable = false,
+                    updatable = false)
+            },
+            inverseJoinColumns = { @JoinColumn(
+                    name = "folder_id",
+                    nullable = false,
+                    updatable = false)
+            })
+    private List<Folder> folders;
+
+    public User(String name) {
+        this.name = name;
+        this.folders = new ArrayList<>();
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Folder> getFolders() {
+        return folders;
+    }
+
+    public void setFolders(List<Folder> folders) {
+        this.folders = folders;
+    }
+}
