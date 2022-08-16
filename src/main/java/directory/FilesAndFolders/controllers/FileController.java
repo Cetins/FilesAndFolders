4f8cf73bbec7662a93c462e4f8f018c0253f9cbc
Cetins <69481095+Cetins@users.noreply.github.com>
2022-08-16 20:44:31 +0100

    folder controller added

--- src/main/java/directory/FilesAndFolders/controllers/FileController.java
+++ src/main/java/directory/FilesAndFolders/controllers/FileController.java
@@ -28,5 +28,5 @@ public class FileController {
         return new ResponseEntity<>(fileRepository.findById(id), HttpStatus.OK);
     }
 
-    
+
 }
---
+++ src/main/java/directory/FilesAndFolders/controllers/FolderController.java
@@ -0,0 +1,31 @@
+package directory.FilesAndFolders.controllers;
+
+import directory.FilesAndFolders.models.Folder;
+import directory.FilesAndFolders.repositories.FolderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class FolderController {
+
+    @Autowired
+    FolderRepository folderRepository;
+
+    @GetMapping(value = "/folders")
+    public ResponseEntity<List<Folder>> getAllFolders() {
+        return new ResponseEntity<>(folderRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/folders/{id}")
+    public ResponseEntity<Optional<Folder>> getFolder(@PathVariable Long id) {
+        return new  ResponseEntity<>(folderRepository.findById(id), HttpStatus.OK);
+    }
+}
